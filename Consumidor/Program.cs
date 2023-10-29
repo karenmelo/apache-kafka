b2855bee83e0a6fb9426a93da978f7a81bd26c2b
Karen <karen_melo@live.com>
2023-10-28 23:29:33 -0300

    first commit about apache kafka

---
+++ Consumidor/Program.cs
@@ -0,0 +1,15 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig { 
+    GroupId = "devIO",
+    BootstrapServers = "localhost:9092" };
+
+using var consumer = new ConsumerBuilder<string, string>(config).Build();
+
+consumer.Subscribe("topic_test");
+
+while (true)
+{
+    var result = consumer.Consume();
+    Console.WriteLine($"Mensagem: {result.Message.Key}-{result.Message.Value}");
+}
---
+++ Produtor/Program.cs
@@ -0,0 +1,15 @@
+﻿using Confluent.Kafka;
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+using var producer = new ProducerBuilder<string, string>(config).Build();
+
+var message = new Message<string, string>
+{
+    Key = Guid.NewGuid().ToString(),
+    Value = $"Teste de envio de mensagem kafka"
+};
+
+var result = await producer.ProduceAsync("topic_test", message);
+
+Console.WriteLine(result);
